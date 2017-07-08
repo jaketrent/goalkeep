3540d48d6cd74c4eec3b43fc6eed9b5f522794ec
jaketrent <trent.jake@gmail.com>
2017-07-08 13:11:33 -0600

    chore(client): cruft app removal

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import css from './App.css'
+import React from 'react'
+import styleable from 'react-styleable'
 
-class App extends Component {
-  render() {
-    return (
-      <div className={css.App}>
-        <div className={css['App-header']}>
-          <img src={logo} className={css['App-logo']} alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className={css['App-intro']}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    )
-  }
-}
+import css from './app.css'
 
-export default App
+export default styleable(css)(props => <div className={props.css.app}>app</div>)
