40216fbdbd240e86ea27bb4966565f9b9f67e5c0
jaketrent <trent.jake@gmail.com>
2017-08-02 06:56:44 -0600

    (list): basic period picker with style and state

--- src/client/goals/index.js
+++ src/client/goals/index.js
@@ -1,14 +1,33 @@
+// @flow
 import { gql, graphql } from 'react-apollo'
 import React from 'react'
 
 import Authed from '../common/ui/authed'
 import Frame from '../common/ui/frame'
+import PeriodPicker from './period-picker'
 
 class Goals extends React.Component {
+  state: {
+    year: ?number,
+    quarter: ?number,
+    week: ?number
+  }
+  state = {}
+  handlePeriodPickerClick = evt => {
+    const [period, num] = evt.target.name.split('-')
+    this.setState(_ => ({ [period]: parseInt(num, 10) }))
+  }
   render() {
     return (
       <Authed>
-        <Frame>goals list</Frame>
+        <Frame>
+          <PeriodPicker
+            year={this.state.year}
+            quarter={this.state.quarter}
+            week={this.state.week}
+            onClick={this.handlePeriodPickerClick}
+          />
+        </Frame>
       </Authed>
     )
   }
---
+++ src/client/goals/period-picker.js
@@ -0,0 +1,68 @@
+// @flow
+import type { Styleable } from '../common/types'
+
+import React from 'react'
+import styleable from 'react-styleable'
+
+import css from './period-picker.css'
+
+type Props = Styleable & {
+  year?: number,
+  quarter?: number,
+  week?: number,
+  onClick: SyntheticEvent => void
+}
+
+const years = [new Date().getFullYear()]
+const quarters = [1, 2, 3, 4]
+const weeks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+const Years = (props: Props) =>
+  <div className={props.css.row}>
+    {years.map(y =>
+      <button
+        key={y}
+        className={y === props.year ? props.css.btnActive : props.css.btn}
+        onClick={props.onClick}
+        name={`year-${y}`}
+      >
+        {y}
+      </button>
+    )}
+  </div>
+
+const Quarters = (props: Props) =>
+  <div className={props.css.row}>
+    {quarters.map(q =>
+      <button
+        key={q}
+        className={q === props.quarter ? props.css.btnActive : props.css.btn}
+        onClick={props.onClick}
+        name={`quarter-${q}`}
+      >
+        {q}
+      </button>
+    )}
+  </div>
+
+const Weeks = (props: Props) =>
+  <div className={props.css.row}>
+    {weeks.map(w =>
+      <button
+        key={w}
+        className={w === props.week ? props.css.btnActive : props.css.btn}
+        onClick={props.onClick}
+        name={`week-${w}`}
+      >
+        {w}
+      </button>
+    )}
+  </div>
+
+export default styleable(css)((props: Props) =>
+  <div className={props.css.picker}>
+    <Years css={props.css} year={props.year} onClick={props.onClick} />
+    <Quarters css={props.css} quarter={props.quarter} onClick={props.onClick} />
+    <Weeks css={props.css} week={props.week} onClick={props.onClick} />
+  </div>
+)
