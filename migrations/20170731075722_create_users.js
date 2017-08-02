f576ed05e9a686d8f2c70e24fbf39cd2cb3d16c2
jaketrent <trent.jake@gmail.com>
2017-08-01 21:06:20 -0600

    feat(db): create users table migration

--- migrations/20170731075722_create_users.js
+++ migrations/20170731075722_create_users.js
@@ -1,3 +1,9 @@
-exports.up = function(knex, Promise) {}
+exports.up = knex =>
+  knex.schema.createTable('users', t => {
+    t.string('email').notNull().primary()
+    t.string('token').notNull()
+    t.dateTime('createdAt').notNull()
+    t.dateTime('updatedAt').notNull()
+  })
 
-exports.down = function(knex, Promise) {}
+exports.down = knex => knex.schema.dropTable('users')
